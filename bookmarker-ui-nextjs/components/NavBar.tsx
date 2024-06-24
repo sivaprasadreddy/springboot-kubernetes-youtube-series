84df5e6f8c6b9745eeedd4f10051ae4504d57431
Raja Kolli <rajadilipkolli@gmail.com>
2024-06-24 06:19:41 +0530

    Upgrade to latest available versions (#32)
    * Update dependency maven-wrapper to v3.3.2 (#75)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.springframework.boot:spring-boot-starter-parent to v3.3.0 (#74)
    
    * Update dependency org.springframework.boot:spring-boot-starter-parent to v3.3.0
    
    * adds missing postgresql
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>
    
    * Update dependency @types/node to v20.14.0 (#68)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency com.google.cloud.tools:jib-maven-plugin to v3.4.3 (#77)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update eclipse-temurin Docker tag to v17.0.11_9-jre-focal (#76)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency maven to v3.9.7 (#78)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @types/node to v20.14.2 (#80)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update testing-library monorepo (#72)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.springframework.boot:spring-boot-starter-parent to v3.3.1 (#83)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency maven to v3.9.8 (#81)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @types/node to v20.14.7 (#82)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update react monorepo (#64)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency axios to v1 (#24)
    
    * Update dependency axios to v1
    
    * adds instructions to install kind
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>
    
    * feat : Update nextjs to V 14
    
    * Update dependency eslint to v9 (#79)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- bookmarker-ui-nextjs/components/NavBar.tsx
+++ bookmarker-ui-nextjs/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => (
     <header>
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
-                <Link href="/"><a className="navbar-brand">Bookmarker</a></Link>
+                <Link className="navbar-brand" href="/">Bookmarker</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarCollapse" aria-controls="navbarCollapse"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +14,7 @@ const NavBar = () => (
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <Link href="/bookmarks/add"><a className="nav-link" >Add Bookmark</a></Link>
+                            <Link className="nav-link" href="/bookmarks/add">Add Bookmark</Link>
                         </li>
                     </ul>
                 </div>
--- kind/READMD.md
+++ kind/READMD.md
@@ -1,3 +1,7 @@
 * Install kinds using go
 
-type `go install sigs.k8s.io/kind@v0.20.0`
+Kind is pre-requisite to start and destroy the cluster
+
+```shell
+go install sigs.k8s.io/kind@v0.23.0
+```

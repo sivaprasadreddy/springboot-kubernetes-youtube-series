10d9840671e96dd06181cde696a48ca30f6d4569
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-08-25 09:58:39 +0530

    Upgrade dependencies

--- bookmarker-api/src/main/java/com/sivalabs/bookmarker/BookmarkerApiApplication.java
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/BookmarkerApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookmarkerApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookmarkerApiApplication.class, args);
 	}
 

5fe48df567f967b8f3937f8eaa6c3f402457b7a1
Raja Kolli <rajadilipkolli@gmail.com>
2025-01-04 17:47:30 +0530

    Upgrade to latest dependencies (#43)
    * Update eclipse-temurin Docker tag to v21.0.5_11-jre (#92)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @testing-library/jest-dom to v6.6.3 (#88)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @types/node to v20.17.10 (#84)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency com.google.cloud.tools:jib-maven-plugin to v3.4.4 (#94)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.springframework.boot:spring-boot-starter-parent to v3.4.1 (#87)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency axios to v1.7.9 (#90)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update nextjs monorepo to v14.2.22 (#86)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @types/node to v20.17.11 (#99)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency eslint to v9.17.0 (#85)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update Node.js to v22 (#95)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update postgres Docker tag to v17 (#97)
    
    * Update postgres Docker tag to v17
    
    * Update BookmarkControllerTest.java
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>
    
    * Update react monorepo to v19 (#98)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Migrate config renovate.json (#100)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update nextjs monorepo to v15 (#96)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @testing-library/react to v16 (#73)
    
    * Update dependency @testing-library/react to v16
    
    * fix : build issue
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- bookmarker-ui-nextjs/next.config.js
+++ bookmarker-ui-nextjs/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   serverRuntimeConfig: {
     API_BASE_URL: process.env.SERVER_SIDE_API_BASE_URL
   },
